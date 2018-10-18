d6f5e2de60fa2d9d61572a480e2a6f1185590868
martaJesztak <jesztakm@gmail.com>
2018-10-18 14:07:18 +0200

    New fields added to Recipe model class.

--- src/main/java/com/mealplanner/app/model/Recipe.java
+++ src/main/java/com/mealplanner/app/model/Recipe.java
@@ -4,14 +4,22 @@ public class Recipe {
 
     private String id;
     private String title;
+    private String image;
+    private String source;
+    private String url;
+    private int yield;
     private String[] ingredients;
     private String instructions;
     private DietLabel[] dietlabels;
     private HealthLabel[] healthLabels;
 
-    public Recipe(String id, String title, String[] ingredients, String instructions, DietLabel[] dietlabels, HealthLabel[] healthLabels) {
+    public Recipe(String id, String title, String image, String source, String url, int yield, String[] ingredients, String instructions, DietLabel[] dietlabels, HealthLabel[] healthLabels) {
         this.id = id;
         this.title = title;
+        this.image = image;
+        this.source = source;
+        this.url = url;
+        this.yield = yield;
         this.ingredients = ingredients;
         this.instructions = instructions;
         this.dietlabels = dietlabels;
@@ -31,6 +39,38 @@ public class Recipe {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public int getYield() {
+        return yield;
+    }
+
+    public void setYield(int yield) {
+        this.yield = yield;
     }
 
     public String[] getIngredients() {
