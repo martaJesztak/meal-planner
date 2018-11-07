0a5a4d045fc6fa82aff5a9a3488050ce3c3a756f
martaJesztak <jesztakm@gmail.com>
2018-11-07 15:12:33 +0100

    List all recipes on main index page saved by user.

--- src/main/java/com/mealplanner/app/controller/MainController.java
+++ src/main/java/com/mealplanner/app/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.mealplanner.app.controller;
 
+import com.mealplanner.app.service.RecipeService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -9,8 +12,12 @@ import java.util.List;
 @Controller
 public class MainController {
 
+    @Autowired
+    RecipeService recipeService;
+
     @GetMapping("/")
-    public String index(){
+    public String index(Model model){
+        model.addAttribute("recipes",recipeService.getAllRecipes());
         return "index";
     }
 
--- src/main/java/com/mealplanner/app/service/RecipeService.java
+++ src/main/java/com/mealplanner/app/service/RecipeService.java
@@ -5,6 +5,8 @@ import com.mealplanner.app.repository.RecipeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class RecipeService {
 
@@ -18,4 +20,8 @@ public class RecipeService {
     public Recipe getOwnRecipe(String id){
         return recipeRepository.findOneById(id);
     }
+
+    public List<Recipe> getAllRecipes() {return recipeRepository.findAll();}
+
+
 }
