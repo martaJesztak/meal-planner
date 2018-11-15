b761422a195f35cb7a89c0d1b704aaf0f1fdf48c
BalazsParducz <parducz.balazs@gmail.com>
2018-11-15 22:08:46 +0100

    uncommented WebSecurityConfig

--- src/main/java/com/mealplanner/app/config/WebSecurityConfig.java
+++ src/main/java/com/mealplanner/app/config/WebSecurityConfig.java
@@ -1,22 +1,29 @@
-package com.mealplanner.app.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-
-import org.springframework.context.annotation.*;
-import org.springframework.security.config.annotation.authentication.builders.*;
-import org.springframework.security.config.annotation.web.configuration.*;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@EnableWebSecurity
-public class WebSecurityConfig implements WebMvcConfigurer {
-
-    @Bean
-    public UserDetailsService userDetailsService() throws Exception {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("asd").roles("USER").build());
-        return manager;
-    }
-}
+//package com.mealplanner.app.config;
+//
+//import org.springframework.context.annotation.*;
+//import org.springframework.core.Ordered;
+//import org.springframework.security.config.annotation.web.configuration.*;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//
+//@EnableWebSecurity
+//public class WebSecurityConfig implements WebMvcConfigurer {
+//
+////    @Bean
+////    public UserDetailsService userDetailsService() throws Exception {
+////        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+////        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("asd").roles("USER").build());
+////        return manager;
+////    }
+//
+////    @Override
+////    public void addViewControllers(ViewControllerRegistry registry) {
+////        WebMvcConfigurer.super.addViewControllers(registry);
+////        registry.addViewController("/login").setViewName("auth/login");
+////        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+////    }
+//}
