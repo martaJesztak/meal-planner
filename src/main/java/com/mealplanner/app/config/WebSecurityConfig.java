81b6c4f7e1b48b7725951d9c1ffab82b2ca48bf5
BalazsParducz <parducz.balazs@gmail.com>
2018-11-19 12:18:27 +0100

    added login html and changed MVC config

--- src/main/java/com/mealplanner/app/config/WebSecurityConfig.java
+++ src/main/java/com/mealplanner/app/config/WebSecurityConfig.java
@@ -1,29 +1,29 @@
-//package com.mealplanner.app.config;
-//
-//import org.springframework.context.annotation.*;
-//import org.springframework.core.Ordered;
-//import org.springframework.security.config.annotation.web.configuration.*;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//
-//@EnableWebSecurity
-//public class WebSecurityConfig implements WebMvcConfigurer {
-//
-////    @Bean
-////    public UserDetailsService userDetailsService() throws Exception {
-////        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-////        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("asd").roles("USER").build());
-////        return manager;
-////    }
-//
-////    @Override
-////    public void addViewControllers(ViewControllerRegistry registry) {
-////        WebMvcConfigurer.super.addViewControllers(registry);
-////        registry.addViewController("/login").setViewName("auth/login");
-////        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
-////    }
-//}
+package com.mealplanner.app.config;
+
+import org.springframework.context.annotation.*;
+import org.springframework.core.Ordered;
+import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@EnableWebSecurity
+public class WebSecurityConfig implements WebMvcConfigurer {
+
+//    @Bean
+//    public UserDetailsService userDetailsService() throws Exception {
+//        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
+//        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("asd").roles("USER").build());
+//        return manager;
+//    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        WebMvcConfigurer.super.addViewControllers(registry);
+        registry.addViewController("/login").setViewName("login");
+        registry.setOrder(Ordered.HIGHEST_PRECEDENCE);
+    }
+}
