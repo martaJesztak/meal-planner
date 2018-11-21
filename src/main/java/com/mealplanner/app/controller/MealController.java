ab843f36af443f7e63fa92e9a8cdb372d8b87bff
martaJesztak <jesztakm@gmail.com>
2018-11-21 13:29:19 +0100

    Link reference added from index page to meal planner page. Meal planner html initiated.

---
+++ src/main/java/com/mealplanner/app/controller/MealController.java
@@ -0,0 +1,11 @@
+package com.mealplanner.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mealplan")
+public class MealController {
+
+
+}
