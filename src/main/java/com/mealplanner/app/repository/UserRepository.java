c07f951a5ca13c65f5c897b09db4e173ad107781
BalazsParducz <parducz.balazs@gmail.com>
2018-12-03 21:19:24 +0100

    added @Repository annotation

--- src/main/java/com/mealplanner/app/repository/UserRepository.java
+++ src/main/java/com/mealplanner/app/repository/UserRepository.java
@@ -2,7 +2,9 @@ package com.mealplanner.app.repository;
 
 import com.mealplanner.app.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends MongoRepository<User, String> {
 
     User findOneById(String id);
