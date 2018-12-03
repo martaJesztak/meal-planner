3e5dced5a0dd8fd157345f3b01aeca2bfcebf8dc
BalazsParducz <parducz.balazs@gmail.com>
2018-12-03 23:56:35 +0100

    new implementation for UserDetailsService

---
+++ src/main/java/com/mealplanner/app/service/MongoUserDetailsService.java
@@ -0,0 +1,40 @@
+package com.mealplanner.app.service;
+
+import com.mealplanner.app.model.User;
+import com.mealplanner.app.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class MongoUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findUserByEmail(username);
+
+        if(user == null) {
+            throw new UsernameNotFoundException("User not found");
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getEmail(),
+                user.getPassword(),
+                isAdmin(username)? Collections.singletonList(new SimpleGrantedAuthority("USER_LIST")) : Collections.emptyList());
+    }
+
+    private boolean isAdmin(String username) {
+        if(username.equals("ADMIN")) {
+            return true;
+        }
+        return false;
+    }
+}
